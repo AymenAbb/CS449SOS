fb128206a64bcf971504b66063a5c2b58eb4f4e8
Aymen <123590232+AymenAbb@users.noreply.github.com>
2025-09-07 22:10:28 -0500

    Line test, Macos again doesnt show anything

--- main.py
+++ main.py
@@ -8,4 +8,9 @@ root.title("Test")
 label = tk.Label(root, text="Hello World", font=("Arial", 18))
 label.pack()
 
+# lines!
+canvas = tk.Canvas(root, width=400, height=120)
+canvas.pack()
+canvas.create_line(10, 60, 390, 60, fill="red", width=2)
+
 root.mainloop()
