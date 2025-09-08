fe999122ade36bc1576387e6cdeefcb2c1b3c3c5
Aymen <123590232+AymenAbb@users.noreply.github.com>
2025-09-07 22:16:50 -0500

    2 line test

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ label.pack()
 # lines!
 canvas = tk.Canvas(root, width=400, height=120)
 canvas.pack()
-canvas.create_line(10, 60, 390, 60, fill="red", width=2)
+canvas.create_line(10, 0, 10, 120, fill="black", width=2)  # left line
+canvas.create_line(390, 0, 390, 120, fill="black", width=2)  # right line
 
 root.mainloop()
