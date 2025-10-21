a9a0e34c573d0a8921fa8e24649bcbf6a0d9dbc7
Aymen <123590232+AymenAbb@users.noreply.github.com>
2025-10-20 20:28:56 -0500

    Unit tests 2 & 3

--- test_sos_game.py
+++ test_sos_game.py
@@ -26,5 +26,47 @@ class TestUserStory1_ChooseBoardSize(unittest.TestCase):
                 self.assertEqual(game.get_cell(row, col), SOSGame.EMPTY)
 
 
+class TestUserStory2_ChooseGameMode(unittest.TestCase):
+
+    def test_ac_2_1_menu_select(self):
+        game_simple = SOSGame(game_mode=SOSGame.SIMPLE)
+        self.assertEqual(game_simple.game_mode, SOSGame.SIMPLE)
+
+        game_general = SOSGame(game_mode=SOSGame.GENERAL)
+        self.assertEqual(game_general.game_mode, SOSGame.GENERAL)
+
+    def test_ac_2_2_post_menu_selection(self):
+        # Rule application
+        game = SOSGame(board_size=4, game_mode=SOSGame.GENERAL)
+
+        self.assertEqual(game.game_mode, SOSGame.GENERAL)
+        self.assertEqual(game.current_player, SOSGame.BLUE)
+
+
+class TestUserStory3_StartNewGame(unittest.TestCase):
+    def test_ac_3_1_reset_state(self):
+        # Reset grid, scores, and becomes blue's turn
+        game = SOSGame(board_size=3)
+        game.make_move(0, 0, "S")
+        game.make_move(1, 1, "O")
+
+        game.reset_game()
+
+        # Grid cleared
+        for row in range(3):
+            for col in range(3):
+                self.assertEqual(game.get_cell(row, col), SOSGame.EMPTY)
+
+        self.assertEqual(game.current_player, SOSGame.BLUE)
+
+    def test_ac_3_2_reconfigure_size_and_mode(self):
+        game = SOSGame(board_size=5, game_mode=SOSGame.SIMPLE)
+
+        game.reset_game(board_size=8, game_mode=SOSGame.GENERAL)
+
+        self.assertEqual(game.board_size, 8)
+        self.assertEqual(game.game_mode, SOSGame.GENERAL)
+
+
 if __name__ == "__main__":
     unittest.main(verbosity=1)
